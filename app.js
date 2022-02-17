d5e3f9499229661f6ef6c60d4b69b588068e2f6c
rharris529 <jordandine@gmail.com>
2022-02-17 17:34:55 -0600

    inquirer added

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page.template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page.template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-    console.log("Portfolio Complete! Checkout index.html to see the output!")
-});
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log("Portfolio Complete! Checkout index.html to see the output!")
+// });
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
