f2072191515ff899891c1ec05e4e27bf4e4cc1a3
rharris529 <jordandine@gmail.com>
2022-02-17 16:12:25 -0600

    generate HTML file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
+const generatePage = require('./src/page.template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) =>
-        console.log(profileItem)
-    );  
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log("Portfolio Complete! Checkout index.html to see the output!")
+});
