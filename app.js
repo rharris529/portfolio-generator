d49c37563592aff3d645dd6e23e3169f4f968094
rharris529 <jordandine@gmail.com>
2022-02-17 18:59:58 -0600

    properly adding new projects

--- app.js
+++ app.js
@@ -39,25 +39,36 @@ const promptUser = () => {
             }
         },
         {
+            type: "confirm",
+            name: "confirmAbout",
+            message: "Would you like to enter some information about yourself for an 'About' section?",
+            default: true
+        },
+        {
             type: "input",
             name: "about",
-            message: "Provide some information about yourself."
-        }
+            message: "Provide some information about yourself:",
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
     ]);
 };
 
 const promptProject = portfolioData => {
-    
-    // if there's no projects array, create one //
-    // if (!portfolioData.projects) {
-    portfolioData.projects = [];
-    // };
-
     console.log(`
     =================
     Add a New Project
     =================
     `);
+    // if there's no projects array, create one //
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
     return inquirer.prompt([
         {
             type: "input",
@@ -116,11 +127,18 @@ const promptProject = portfolioData => {
             message: "Would you like to enter another project?",
             default: false
         }
-    ]);    
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    }); 
 };
 
 promptUser()
-.then(answers => console.log(answers))
 .then(promptProject)
 .then(portfolioData => {
     console.log(portfolioData);    
